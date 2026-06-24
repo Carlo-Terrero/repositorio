10caa9adcd915b44a337558d2200eabeef6c5e3b
Carlos Terrero <carlosjose2111@gmail.com>
2026-06-24 18:10:46 +0200

    eliminacion de coments, agragacion de img y link el el readme

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './componenets/App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 
-// import "./style/main.scss";
 import './style/main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
